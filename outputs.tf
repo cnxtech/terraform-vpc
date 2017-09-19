05180bc8384506b7a660a3c4cd648c30d62d6a86
Vaughan Jones <vaughanlewisjones@icloud.com>
2017-09-19 18:32:04 +0100

    Add additional outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "subnet_ids" {
   description = "List of subnet identifiers"
   value       = "${aws_subnet.public.*.id}"
 }
+
+output "igw_id" {
+  description = "Internet gateway resource identifier"
+  value       = "${aws_internet_gateway.main.id}"
+}
+
+output "route_table_id" {
+  description = "Route table resource identifier"
+  value       = "${aws_route_table.main.id}"
+}
